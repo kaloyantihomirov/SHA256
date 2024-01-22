21c7f5b96082f40d8aaee9b88bfdada2a012cb31
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2024-01-22 15:29:37 +0200

    implemented the function that takes the input string and converts all characters to their ASCII binary equivalents

--- main.cpp
+++ main.cpp
@@ -61,7 +61,6 @@ vector<unsigned long> resize_block(vector<unsigned long>);
 string compute_hash(const vector<unsigned long>);
 
 // Taken from NIST spec.
-
 #define ROTRIGHT(word,bits) (((word) >> (bits)) | ((word) << (32-(bits))))
 #define SSIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
 #define SSIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
@@ -70,3 +69,31 @@ string compute_hash(const vector<unsigned long>);
 
 #define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
 #define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
+
+vector<unsigned long> convert_to_binary(const string input)
+{
+    vector<unsigned long> block;
+
+    for (int i = 0; i < input.size(); i++)
+    {
+        bitset<8> b(input.c_str()[i]);
+//        cout << b << endl;
+//        cout << b.to_ulong() << endl;
+        block.push_back(b.to_ulong());
+    }
+
+    return block;
+}
+
+int main()
+{
+    vector<unsigned long> binary = convert_to_binary("abc");
+
+    for (int i = 0; i < binary.size(); i++)
+    {
+        cout << binary[i] << '\t';
+    }
+
+    return 0;
+}
+
